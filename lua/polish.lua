c2946cc9c450b02621221ef8ab40fde3af076199
DolphyWind <yunusaydin590@gmail.com>
2024-05-05 20:24:50 +0300

    Move large plugin configs to their own files

--- lua/polish.lua
+++ lua/polish.lua
@@ -38,6 +38,7 @@ lspconfig.clangd.setup {
 lspconfig.jdtls.setup {}
 
 require("presence").setup {
+  enabled = false,
   -- General options
   auto_update = true, -- Update activity based on autocmd events (if `false`, map or manually execute `:lua package.loaded.presence:update()`)
   neovim_image_text = "The One True Text Editor", -- Text displayed when hovered over the Neovim image
@@ -45,7 +46,7 @@ require("presence").setup {
   log_level = nil, -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
   debounce_timeout = 10, -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
   enable_line_number = false, -- Displays the current line number instead of the current project
-  blacklist = { "CodeGuessing", "Code_Guessing" }, -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
+  blacklist = { "CodeGuessing", "Code_Guessing", "Homeworks"}, -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
   buttons = true, -- Configure Rich Presence button(s), either a boolean to enable/disable, a static table (`{{ label = "<label>", url = "<url>" }, ...}`, or a function(buffer: string, repo_url: string|nil): table)
   file_assets = {}, -- Custom file asset definitions keyed by file names and extensions (see default config at `lua/presence/file_assets.lua` for reference)
   show_time = true, -- Show the timer
