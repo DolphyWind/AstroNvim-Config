3bdd49a0e80bf5ea6fb7c6f5cb2b62d33b5abac5
dolphywind <yunusaydin590@gmail.com>
2025-09-09 17:15:17 +0300

    Oops

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -3,23 +3,6 @@
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
 --       as this provides autocomplete and documentation while editing
 
-function debug_table_as_str(tbl, indent)
-  indent = indent or 0
-  local spacing = string.rep(" ", indent)
-  local result = ""
-
-  for key, value in pairs(tbl) do
-    if type(value) == "table" then
-      result = result .. spacing .. "[" .. tostring(key) .. "] => Table:\n"
-      result = result .. debug_table_as_str(value, indent + 1)
-    else
-      result = result .. spacing .. "[" .. tostring(key) .. "] => " .. tostring(value) .. "\n"
-    end
-  end
-
-  return result
-end
-
 ---@type LazySpec
 return {
   "AstroNvim/astrocore",
@@ -27,10 +10,10 @@ return {
   opts = {
     -- Configure core features of AstroNvim
     features = {
-      large_buf = { size = 1024 * 500, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
+      large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
       autopairs = true, -- enable autopairs at start
       cmp = true, -- enable completion at start
-      diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
+      diagnostics = { virtual_text = true, virtual_lines = false }, -- diagnostic settings on startup
       highlighturl = true, -- highlight URLs at start
       notifications = true, -- enable notifications at start
     },
@@ -38,6 +21,19 @@ return {
     diagnostics = {
       virtual_text = true,
       underline = true,
+    },
+    -- passed to `vim.filetype.add`
+    filetypes = {
+      -- see `:h vim.filetype.add` for usage
+      extension = {
+        -- foo = "fooscript",
+      },
+      filename = {
+        -- [".foorc"] = "fooscript",
+      },
+      pattern = {
+        -- [".*/etc/foo/.*"] = "fooscript",
+      },
     },
     -- vim options can be configured here
     options = {
@@ -54,12 +50,12 @@ return {
         -- configure global vim variables (vim.g)
         -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
         -- This can be found in the `lua/lazy_setup.lua` file
-        autoformat_enabled = true,
-        cmp_enabled = true, -- enable completion at start
-        autopairs_enabled = true, -- enable autopairs at start
-        diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
-        icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
-        ui_notifications_enabled = true, -- disable notifications when toggling UI elements
+        -- autoformat_enabled = true,
+        -- cmp_enabled = true, -- enable completion at start
+        -- autopairs_enabled = true, -- enable autopairs at start
+        -- diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
+        -- icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
+        -- ui_notifications_enabled = true, -- disable notifications when toggling UI elements
       },
     },
     -- Mappings can be configured through AstroCore as well.
@@ -92,14 +88,17 @@ return {
         -- this is useful for naming menus
         ["<Leader>b"] = { desc = "Buffers" },
         ["<Leader>m"] = { desc = "Custom" },
-        ["<Leader>ma"] = {"<cmd>AerialNavToggle<CR>", desc = "Toggle Aerial Nav"},
-        ["<Leader>mm"] = {function ()
-          if vim.opt.relativenumber:get() then
-            vim.opt.relativenumber = false
-          else
-            vim.opt.relativenumber = true
-          end
-        end, desc = "Toggle relativenumber"},
+        ["<Leader>ma"] = { "<cmd>AerialNavToggle<CR>", desc = "Toggle Aerial Nav" },
+        ["<Leader>mm"] = {
+          function()
+            if vim.opt.relativenumber:get() then
+              vim.opt.relativenumber = false
+            else
+              vim.opt.relativenumber = true
+            end
+          end,
+          desc = "Toggle relativenumber",
+        },
 
         ["<Leader>mp"] = { "<cmd>Copilot panel<CR>", desc = "Copilot Panel" },
         ["<Leader>mc"] = { "<cmd>Copilot disable<CR>", desc = "Disable copilot" },
